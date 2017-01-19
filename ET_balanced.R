6ee8dc20d407490a63f67360ec7c9e588b67a036
mgwalsh <markusgwalsh@gmail.com>
2017-01-19 14:10:15 +0300

    documentation update

--- ET_balanced.R
+++ ET_balanced.R
@@ -81,11 +81,13 @@ gidy <- ifelse(xy$y<0, paste("S", ygid, sep=""), paste("N", ygid, sep=""))
 GID <- paste(gidx, gidy, sep="-")
 xy <- cbind(xy, GID)
 
-# project sample coordinates to longlat
+# plot results
 coordinates(xy) <- ~x+y
 crs(xy) <- "+proj=laea +ellps=WGS84 +lon_0=20 +lat_0=5 +units=m +no_defs"
-plot(crop(roi, xy)) ## plot cropped ROI grid
-plot(xy, col="red", add = T)   ## overlay sample points
+plot(crop(roi, xy))         ## plot cropped ROI grid
+plot(xy, col="red", add=T)  ## overlay sample points
+
+# project sample coordinates to longlat
 ET_locs_LL <- as.data.frame(spTransform(xy, CRS("+proj=longlat +datum=WGS84")))
 colnames(ET_locs_LL)[1:3] <- c("GID","Lon","Lat")
 
