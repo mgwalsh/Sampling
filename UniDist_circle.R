a816a51130be38b90aa29d4f4bbc4dd07dfdf400
mgwalsh <markusgwalsh@gmail.com>
2017-08-22 08:19:16 +0300

    Uniform point distribution in a circle

--- UniDist_circle.R
+++ UniDist_circle.R
@@ -1,4 +1,5 @@
 # Uniform point distribution in a circle of a given area
+# M. Walsh, Aug. 2017
 
 area <- 100 # area in square meters
 n <- 10000
@@ -6,4 +7,5 @@ r <- sqrt(runif(n))*sqrt(area/pi) # random radius
 t <- runif(n, 0, 2*pi) # random angle
 x <- r * cos(t) # x coordinate
 y <- r * sin(t) # y coordinate
+par(pty="s")
 plot(x, y, pch=3, cex=0.6)
