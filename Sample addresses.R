00d899907005dea090e1803bff8938d0643fb2aa
mgwalsh <markusgwalsh@gmail.com>
2017-09-07 04:26:36 +0300

    TZ what3words soil sample address geocoding

---
+++ Sample addresses.R
@@ -0,0 +1,25 @@
+# TZ what3words soil sample address geocoding
+# M. Walsh, September 2017
+
+# obtain a what3words API key @ https://accounts.what3words.com
+# install.packages(c("downloader","threewords")), dependencies=TRUE)
+require(downloader)
+require(threewords)
+
+# Data downloads -----------------------------------------------------------
+# set working directory
+dir.create("TZ_w3w", showWarnings=F)
+setwd("./TZ_w3w")
+
+# download MobileSurvey data
+download("https://www.dropbox.com/s/5yp23mlyhx8irp8/Soil_sample_2017_09_06.csv.zip?raw=1", "Soil_sample_2017_09_06.csv.zip", mode="wb")
+unzip("Soil_sample_2017_09_06.csv.zip", overwrite=T)
+samp <- read.table("Soil_sample_2017_09_06.csv", header=T, sep=",")
+
+# Data setup --------------------------------------------------------------
+# convert lat/lon to numeric and restrict to samples located south of the equator
+samp$lat <- as.numeric(as.character(samp$lat))
+samp$lon <- as.numeric(as.character(samp$lon))
+samp <- samp[samp$lat < 0,]
+
+test <- from_position(key = "TE2QLEOP", positions = c(6.385336,-36.293769))
