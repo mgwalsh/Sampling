2507c1beef797581999fffb1a9568e9360443564
mgwalsh <markusgwalsh@gmail.com>
2017-09-08 01:56:29 +0300

    TZ what3words resolution sensitive soil sample address geocoding

--- Sample addresses.R
+++ Sample addresses.R
@@ -1,10 +1,13 @@
 # TZ what3words soil sample address geocoding
-# W.Wu, J.Chen & M.Walsh, September 2017
+# M. Walsh, W. Wu & J. Chen, September 2017
 
 # obtain a what3words API key @ https://accounts.what3words.com
-# install.packages(c("downloader","threewords"), dependencies=TRUE)
-require(downloader)
-require(threewords)
+# install.packages(c("downloader","rgdal","threewords"), dependencies=TRUE)
+suppressPackageStartupMessages({
+  require(downloader)
+  require(rgdal)
+  require(threewords)
+})
 
 # Data downloads -----------------------------------------------------------
 # set working directory
@@ -23,11 +26,24 @@ samp$lon <- as.numeric(as.character(samp$lon))
 samp <- samp[samp$lat < 0,]
 samp <- na.omit(samp)
 
+# project test data to grid CRS
+samp.proj <- as.data.frame(project(cbind(samp$lon, samp$lat), "+proj=laea +ellps=WGS84 +lon_0=20 +lat_0=5 +units=m +no_defs"))
+colnames(samp.proj) <- c("x","y")
+samp <- cbind(samp, samp.proj)
+
+# set desired location resolution in meters 
+res.pixel <- 100
+lx <- ceiling(samp$x/res.pixel)*res.pixel
+ly <- ceiling(samp$y/res.pixel)*res.pixel
+
+# project to longlat
+ll <- project(cbind(lx, ly), "+proj=laea +ellps=WGS84 +lon_0=20 +lat_0=5 +units=m +no_defs", inv=TRUE)
+
 # What3Words --------------------------------------------------------------
-# very slow for this big file but you can always <esc>
-w3w <- matrix(NA, dim(samp)[1], 3)
-for(i in 1:dim(samp)[1]){ 
-  w3w[i,] <- from_position(key = "TE2QLEOP", positions = c(samp[i,"lat"], samp[i,"lon"]))$words
+# quite slow for this big file but you can always <esc>
+w3w <- matrix(NA, dim(ll)[1], 3)
+for(i in 1:dim(ll)[1]){ 
+  w3w[i,] <- from_position(key = "TE2QLEOP", positions = c(ll[i,"ly"], ll[i,"lx"]))$words
   print(w3w[i,])
 }
 colnames(w3w) <- c("w1","w2","w3")
