612150cb3386ebadb1a91d2af385cd4414104f18
mgwalsh <markusgwalsh@gmail.com>
2017-11-12 15:03:39 +0300

    Geographically balanced sampling setup for MobileSurvey survey in Kebbie State, Nigeria

--- Kebbie_balanced.R
+++ Kebbie_balanced.R
@@ -37,11 +37,11 @@ rmask <- index[which(index$index == 1),]
 # Geographically balanced sampling ----------------------------------------
 # set sampling parameters
 N <- nrow(rmask) ## Population size (in 250 m pixels)
-n <- round(N/8*0.025,0) ## Set sample size (number of sampling locations)
+n <- round(N/8*0.01,0) ## Set sample size (number of sampling locations)
 p <- rep(n/N,N)  ## Inclusion probabilities
 
 # draw geographically balanced sample
-set.seed(6405)                      ## sets reapeatable randomization seed
+set.seed(6405)                      ## sets repeatable randomization seed
 B <- cbind(p, rmask[,1], rmask[,2]) ## specifies balancing variables
 rsamp <- cube(p, B)                 ## cube samples from population
 
