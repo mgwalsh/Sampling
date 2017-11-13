b672edc76ace4f9a27ecd1798d216dd4dfb83ee1
mgwalsh <markusgwalsh@gmail.com>
2017-11-13 09:54:07 +0300

    Geographically balanced sampling setup for MobileSurvey in Tanzania

--- TZ_balanced.R
+++ TZ_balanced.R
@@ -68,7 +68,7 @@ sloc <- spTransform(xy, CRS(proj4string(shape)))
 gadm <- sloc %over% shape
 sloc <- as.data.frame(sloc)
 samp <- cbind(gadm[ ,c(5,7,9)], sloc)
-colnames(samp) <- c("State", "District", "Ward", "Lon", "Lat")
+colnames(samp) <- c("Region", "District", "Ward", "Lon", "Lat")
 
 # Write file --------------------------------------------------------------
 write.csv(samp, "TZ_sample.csv", row.names = F)
