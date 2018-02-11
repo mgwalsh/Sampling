4d291ee98f0fbfc62e74c4d3217f6b7452d74bf2
mgwalsh <markusgwalsh@gmail.com>
2018-02-11 11:08:24 +0300

    Spatially balanced sampling setup for MobileSurvey in Nigeria

--- NG_balanced.R
+++ NG_balanced.R
@@ -45,7 +45,7 @@ rmask <- index[which(index$index == 1),]
 # Geographically balanced sampling ----------------------------------------
 # set sampling parameters
 N <- nrow(rmask) ## population size (in 250 m pixels)
-n <- round(N/8*0.025,0) ## set sample size (number of sampling locations)
+n <- round(N/16*0.05,0) ## set sample size (number of sampling locations)
 p <- rep(n/N,N)  ## inclusion probabilities
 
 # draw geographically balanced sample
