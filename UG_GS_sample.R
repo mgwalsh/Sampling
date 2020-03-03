111b341d2dba7533d13b45e45d4b1bc928ee52e7
mgwalsh <markusgwalsh@gmail.com>
2020-03-03 14:48:19 +0300

    Spatially balanced sampling setup for GeoSurvey of Uganda

---
+++ UG_GS_sample.R
@@ -0,0 +1,77 @@
+# Spatially balanced sampling setup for GeoSurvey of Uganda
+# M. Walsh, December 2019
+
+# install.packages(c("downloader","rgdal","raster","BalancedSampling","leaflet","htmlwidgets"), dependencies=T)
+suppressPackageStartupMessages({
+  require(downloader)
+  require(rgdal)
+  require(raster)
+  require(sp)
+  require(BalancedSampling)
+  require(leaflet)
+  require(htmlwidgets)
+})
+
+# Data setup --------------------------------------------------------------
+# create a data folder in your current working directory
+dir.create("UG_GS_sample", showWarnings=F)
+setwd("./UG_GS_sample")
+dir.create("Results", showWarnings=F)
+
+# download sampling frame
+download("https://osf.io/exk7m?raw=1", "UG_GS_sampling_frame.zip", mode="wb")
+unzip("UG_GS_sampling_frame.zip", overwrite=T)
+shape <- shapefile("gadm36_UGA_4.shp")
+glist <- list.files(pattern="tif", full.names=T)
+grids <- stack(glist)
+
+# Sample setup ------------------------------------------------------------
+# create a ROI image
+cpt <- 1    ## set land mask to 1
+roi <- overlay(grids, fun=function(x) {return(ifelse(x[1] >= cpt, 1, 0))})
+plot(roi, axes=F, legend=F)
+
+# extract ROI coordinates
+coord <- coordinates(roi)
+index <- extract(roi, coord)
+index <- as.data.frame(cbind(coord, index))
+rmask <- index[which(index$index == 1),]
+
+# Geographically balanced sampling ----------------------------------------
+# set sampling parameters
+N <- nrow(rmask) ## population size (in 250 m pixels)
+n <- round(N/16*0.05,0) ## set sample size (number of sampling locations)
+p <- rep(n/N,N)  ## inclusion probabilities
+
+# draw geographically balanced sample
+set.seed(6405)                      ## sets repeatable randomization seed
+B <- cbind(p, rmask[,1], rmask[,2]) ## specifies balancing variables
+rsamp <- cube(p, B)                 ## samples from population
+
+# plot sample result
+plot(roi, axes=F, legend=F)
+points(rmask[rsamp,1], rmask[rsamp,2], pch=3, col="red", cex=0.2)
+
+# Write files -------------------------------------------------------------
+# extract sample coordinates
+x <- rmask[rsamp,1]
+y <- rmask[rsamp,2]
+xy <- data.frame(cbind(x,y))
+
+# attach GADM-L2 and above unit names from shape
+coordinates(xy) <- ~x+y
+crs(xy) <- "+proj=laea +ellps=WGS84 +lon_0=20 +lat_0=5 +units=m +no_defs"
+sloc <- spTransform(xy, CRS(proj4string(shape)))
+gadm <- sloc %over% shape
+sloc <- as.data.frame(sloc)
+samp <- cbind(gadm[ ,c(4,7)], sloc)
+colnames(samp) <- c("Province", "District", "lon", "lat")
+write.csv(samp, "ZM_GS_sample.csv", row.names = F)
+
+# Sampling map widget -----------------------------------------------------
+# render map
+w <- leaflet() %>% 
+  addProviderTiles(providers$OpenStreetMap.Mapnik) %>%
+  addCircleMarkers(samp$lon, samp$lat, clusterOptions = markerClusterOptions())
+w ## plot widget 
+saveWidget(w, 'ZM_GS_sample.html', selfcontained = T)
